c5f01e7389839a305c830085bfd8f3f1573c23eb
Daniel-20051 <1234ayokule@gmail.com>
2025-07-18 10:13:00 +0100

    slight-adjustment

--- src/Components/DateSeparator.tsx
+++ src/Components/DateSeparator.tsx
@@ -1,6 +1,6 @@
 const DateSeparator = ({ date }: { date: string }) => (
   <div className="flex justify-center my-2">
-    <span className="bg-gray-200 text-gray-600 text-xs px-3 py-1 rounded-full">
+    <span className="bg-gray-200 text-gray-600 text-[10px] px-2 py-1 rounded-full">
       {date}
     </span>
   </div>
